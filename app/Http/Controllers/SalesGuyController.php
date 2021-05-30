27f4d56d52b278ead5a83134c9df306abf417b4c
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-30 14:39:57 +0500

    creating sales guy controller

---
+++ app/Http/Controllers/SalesGuyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SalesGuy;
+use Illuminate\Http\Request;
+
+class SalesGuyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SalesGuy  $salesGuy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SalesGuy $salesGuy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SalesGuy  $salesGuy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SalesGuy $salesGuy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SalesGuy  $salesGuy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SalesGuy $salesGuy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SalesGuy  $salesGuy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SalesGuy $salesGuy)
+    {
+        //
+    }
+}
