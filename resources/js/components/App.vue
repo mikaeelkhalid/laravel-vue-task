10aa15481356a798f11e9389ec43adb220564163
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-31 02:20:19 +0500

    adding jumbotron and importing NavBar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,18 +1,34 @@
 <template>
-    <DataTable />
-
-    <SelectDropDown />
+    <div class="container">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Sales Report</h1>
+                <p class="lead">Local Hero Laravel + Vue Assesment</p>
+            </div>
+        </div>
+        <div class="mt-5 mb-5">
+            <NavBar />
+        </div>
+        <div class="container mt-5">
+            <SelectDropDown />
+        </div>
+        <div class="container mt-5">
+            <DataTable />
+        </div>
+    </div>
 </template>
 
 <script>
 import DataTable from "./DataTable.vue";
 import SelectDropDown from "./SelectDropDown.vue";
+import NavBar from "./NavBar.vue";
 
 export default {
     name: "App",
     components: {
         DataTable,
         SelectDropDown,
+        NavBar,
     },
 };
 </script>
@@ -21,7 +37,6 @@ export default {
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     text-align: center;
-    color: #2c3e50;
     margin-top: 60px;
 }
 </style>
