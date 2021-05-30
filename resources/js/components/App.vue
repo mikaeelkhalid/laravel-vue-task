16beb01de14057171c2267c0710abba92567f727
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-30 20:41:31 +0500

    uninstall vue-multiselect

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,6 @@
 <template>
     <DataTable />
+
     <SelectDropDown />
 </template>
 
--- resources/js/components/SelectDropDown.vue
+++ resources/js/components/SelectDropDown.vue
@@ -1,9 +1,51 @@
 <template>
-    <h1>hello from drop down table</h1>
+    <div>
+        <label class="typo__label">Simple select / dropdown</label>
+        <multiselect
+            v-model="value"
+            :options="options"
+            :multiple="true"
+            :close-on-select="false"
+            :clear-on-select="false"
+            :preserve-search="true"
+            placeholder="Pick some"
+            label="name"
+            track-by="name"
+            :preselect-first="true"
+        >
+            <template slot="selection" slot-scope="{ values, search, isOpen }"
+                ><span
+                    class="multiselect__single"
+                    v-if="values.length &amp;&amp; !isOpen"
+                    >{{ values.length }} options selected</span
+                ></template
+            >
+        </multiselect>
+        <pre class="language-json"><code>{{ value  }}</code></pre>
+    </div>
 </template>
 
 <script>
-export default {};
+import Multiselect from "vue-multiselect";
+
+export default {
+    components: {
+        Multiselect,
+    },
+    data() {
+        return {
+            value: [],
+            options: [
+                { name: "Vue.js", language: "JavaScript" },
+                { name: "Adonis", language: "JavaScript" },
+                { name: "Rails", language: "Ruby" },
+                { name: "Sinatra", language: "Ruby" },
+                { name: "Laravel", language: "PHP" },
+                { name: "Phoenix", language: "Elixir" },
+            ],
+        };
+    },
+};
 </script>
 
-<style></style>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
