abbea7018dd3a72fb3704513abff7bbb53108175
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-31 02:21:33 +0500

    creating navbar

---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="nav nav-pills justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Add Data</a>
+        </li>
+    </ul>
+</template>
