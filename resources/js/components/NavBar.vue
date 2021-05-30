873a89f9966de8a14ecdc7c6dca5031405a60b0a
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-31 02:31:29 +0500

    color fix

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,10 +1,18 @@
 <template>
     <ul class="nav nav-pills justify-content-center">
         <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#"> <span class="NavColor">Home</span></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Add Data</a>
+            <a class="nav-link" href="#"
+                ><span class="NavColor">Add Data</span>
+            </a>
         </li>
     </ul>
 </template>
+
+<style>
+.NavColor {
+    color: green;
+}
+</style>
