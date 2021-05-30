6072530828ebb605dabf9b6678359f1597b6ec49
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-30 19:39:33 +0500

    registering DataTable and SelectDropDown components into App

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,7 +1,26 @@
 <template>
-    <h1>hello from vue js</h1>
+    <DataTable />
+    <SelectDropDown />
 </template>
 
-<script></script>
+<script>
+import DataTable from "./DataTable.vue";
+import SelectDropDown from "./SelectDropDown.vue";
 
-<style></style>
+export default {
+    name: "App",
+    components: {
+        DataTable,
+        SelectDropDown,
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
