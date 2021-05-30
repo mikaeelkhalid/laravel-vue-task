0e57668d66d9e0a7bff6e5343d2b5d137ec1995f
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-30 17:38:46 +0500

    calling sale and sales guy seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            SalesGuySeeder::class,
+            SaleSeeder::class,
+            
+        ]);
     }
 }
