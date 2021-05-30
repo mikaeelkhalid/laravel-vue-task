c741d18153cd5ee4e75bb711885edf5a8877c506
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-30 14:41:39 +0500

    creating sales migration

---
+++ database/migrations/2021_05_30_091344_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sales_guy_id');
+            $table->foreign('sales_guy_id')->references('id')->on('sales_guys')->onDelete('cascade');
+            $table->string("postal_code")->unique();
+            $table->string("stats");
+            $table->string("category");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
