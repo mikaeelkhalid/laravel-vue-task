f57a9659bc805606446ce7800be0a56ddd28f902
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-30 14:39:19 +0500

    creating postal code conflict controller

---
+++ app/Http/Controllers/PostalCodeConflictValidatorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostalCodeConflictValidator;
+use Illuminate\Http\Request;
+
+class PostalCodeConflictValidatorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PostalCodeConflictValidator  $postalCodeConflictValidator
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PostalCodeConflictValidator $postalCodeConflictValidator)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PostalCodeConflictValidator  $postalCodeConflictValidator
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PostalCodeConflictValidator $postalCodeConflictValidator)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PostalCodeConflictValidator  $postalCodeConflictValidator
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PostalCodeConflictValidator $postalCodeConflictValidator)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PostalCodeConflictValidator  $postalCodeConflictValidator
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PostalCodeConflictValidator $postalCodeConflictValidator)
+    {
+        //
+    }
+}
