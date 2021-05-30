80e5021d98f9c62b3773b78aa4437d353ebe0365
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-05-31 02:22:18 +0500

    creating data table view

--- resources/js/components/DataTable.vue
+++ resources/js/components/DataTable.vue
@@ -1,9 +1,67 @@
 <template>
-    <h1>hello</h1>
+    <div>
+        <table class="table table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+                <tfoot class="table-dark">
+                    <tr>
+                        <th>total</th>
+                        <td>212</td>
+                        <td>121</td>
+                        <td>43</td>
+                    </tr>
+                </tfoot>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+export default {
+    data(){
+        
+        return{
+            categories: [],
+            errors: [] 
+        }
+        
+    },
+    async created() {
+    try {
+      const response = await axios.get(`http://localhost:8000/api/get-categories`)
+      this.categories = response.data
+      console.log(this.categories)
+    } catch (e) {
+      this.errors.push(e)
+    }
+  }
+};
 </script>
 
 <style></style>
